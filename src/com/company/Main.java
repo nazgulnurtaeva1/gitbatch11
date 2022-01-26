baaa111626226bec437885df998126ccdf176cf9
nazgulnurtaeva1 <nazgulnurtaeva1@gmail.com>
2022-01-25 20:56:04 -0500

    3rd commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,5 +3,6 @@ package com.company;
 public class Main {
     public static void main(String[] args) {
         System.out.println("I am Batch 11 student");
+        System.out.println("I am Batch 12 student");
     }
 }
